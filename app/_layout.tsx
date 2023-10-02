17f0dbf174049f28ff7d1423853ffde63a8fd428
xatrarana <chhatraclas@gmail.com>
2023-10-02 21:47:29 +0545

    init to win

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { Slot, SplashScreen, Stack, useRouter, useSegments } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { ClerkProvider,useAuth } from "@clerk/clerk-expo";
+import * as SecureStore from 'expo-secure-store';
+
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -42,15 +45,53 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+const tokenCache = {
+  async getToken(key:string){
+    try {
+      return  SecureStore.getItemAsync(key);
+    } catch (error) {
+      console.log(error);
+      return
+    }
+  },
+  async saveToken(key:string, value:string){
+    try {
+      return  SecureStore.setItemAsync(key,value)
+    } catch (error) {
+      console.log(error);
+      return 
+    }
+  }
+}
+
+const InitialLayout = ()=>{
+  const {isLoaded,isSignedIn} = useAuth()
+  const segments = useSegments();
+  const router = useRouter()
+  useEffect(()=>{
+  if(!isLoaded) return
+    const inTabsGroup = segments[0] === '(auth)';
+    console.log(inTabsGroup);
+    
+    if(isSignedIn && !inTabsGroup){
+      router.replace('/home')
+    }else if(!isSignedIn){
+        router.replace('/login')
+    }
+    console.log('isSignedIn',isSignedIn);
+    
+  },[isSignedIn])
+  return <Slot/>
+}
+
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+<InitialLayout/>
+      </ClerkProvider>
     </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ActivityIndicator, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+        <ActivityIndicator size={"large"} ></ActivityIndicator>
+    </View>
+  )
+}
+
+export default index
